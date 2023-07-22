964db1ea29a343a6a8d5578c666a5d2be3c4df15
sanavalos <santiago.avalos@outlook.com>
2023-07-22 16:31:00 -0300

    refactor: get 10 upcoming holidays

--- src/commands/getHolidaysCommand.js
+++ src/commands/getHolidaysCommand.js
@@ -14,22 +14,20 @@ module.exports = async (message) => {
   } else {
     const selectedCountry = countries.filter((c) => c.iso === country);
     if (selectedCountry?.length > 0) {
-     try {
-      const resp = await axios.get(`https://calendarific.com/api/v2/holidays?api_key=${process.env.HOLIDAYS_API_KEY}&country=${selectedCountry[0].iso}&year=${currentYear}`);
-      let holidays = await resp.data.response.holidays;
-      holidaysArray = holidays.filter((h) => new Date(h.date.iso) > currentDate);
-      holidaysString = holidaysArray.map((h) => `- ${h.name} on ${h.date.iso}`).join(' \n');
-      if (holidaysString.length > 4000) {
-        holidays = holidaysArray.slice(0, 10);
+      const isoCountry = selectedCountry[0].iso;
+      try {
+       const resp = await axios.get(`https://calendarific.com/api/v2/holidays?api_key=${process.env.HOLIDAYS_API_KEY}&country=${isoCountry}&year=${currentYear}`);
+       let holidays = await resp.data.response.holidays;
+       holidaysArray = holidays.filter((h) => new Date(h.date.iso) > currentDate).slice(0, 10)
+       holidaysString = holidaysArray.map((h) => `- ${h.name} on ${h.date.iso}`).join(' \n');
+       message.reply({
+         content: `The upcoming holidays for ${selectedCountry[0].country_name} are : \n${holidaysString}`,
+       });
+      } catch (error) {
+        message.reply({
+          content: 'Oops! Something went wrong. Please try again.',
+        });
       }
-      message.reply({
-        content: `The remaining holidays for ${selectedCountry[0].country_name} are : \n${holidays.map((h) => `- ${h.name} on ${h.date.iso}`).join(' \n')}`,
-      });
-     } catch (error) {
-      message.reply({
-       content: 'Oops! Something went wrong. Please try again.',
-     });
-     }
     } else {
       message.reply({
         content: 'The country code is invalid. Please try again.',
